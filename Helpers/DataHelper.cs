7915a29e022ab72db1b85492be744b43d7e1fab6
Leonardo Bressan <leo_bressan@icloud.com>
2017-11-27 21:41:06 -0200

    Ajusta modelo de item e cria geracao de itens

--- Helpers/DataHelper.cs
+++ Helpers/DataHelper.cs
@@ -2,6 +2,7 @@ using System;
 using System.Linq;
 using System.Collections.Generic;
 using RPG_New.Inimigos;
+using RPG_New.Itens;
 
 namespace RPG_New
 {
@@ -12,7 +13,6 @@ namespace RPG_New
         public static Inimigo GetMonster(int lvl)
         {
             Random rnd = new Random();
-
 
             List<Inimigo> inimigos = new List<Inimigo>
             {
@@ -26,10 +26,42 @@ namespace RPG_New
                 new Inimigo(7, "Troll", lvl),
                 new Inimigo(8, "Ciclope", lvl),
                 new Inimigo(9, "Elemental", lvl)
-
             };
 
             return inimigos[rnd.Next(0, inimigos.Count-1)];
+        }
+
+        public static Item GetItem()
+        {
+            Random rnd = new Random();
+
+            List<Item> itens = new List<Item>
+            {
+                new Item("Pote de Vida Pequeno", 2, 1, 10, 0, 0, 0, 30),
+                new Item("Pote de vida Médio", 2, 2, 10, 0, 0, 0, 60),
+                new Item("Pote de vida Grande", 2, 3, 10, 0, 0, 0, 120),
+                new Item("Espada de Madeira", 1, 1, 20, 5, 0, 0, 0),
+                new Item("Espada de Aço", 1, 2, 20, 40, 0, 0, 0),
+                new Item("Espada de Diamante", 1, 3, 30, 20, 0, 0, 0),
+                new Item("Armadura de Madeira", 1, 1, 20, 0, 2, 3, 0),
+                new Item("Armadura de Aço", 1, 2, 20, 0, 4, 6, 0),
+                new Item("Armadura de Diamante", 1, 3, 20, 0, 8, 12, 0)
+
+            };
+
+            //Porcentagem para encontrar itens
+            int facil = 70;
+            int medio = 20;
+            //Dificil = 10%
+
+            int probItens = rnd.Next(1, 100);
+
+            int dificuldade = (probItens < facil ? 1 : (probItens < (facil + medio) ? 2 : 3));
+
+            List<Item> ListDificuldade = itens.Where(i => i.Dificuldade == dificuldade).ToList();
+
+            return ListDificuldade[rnd.Next(0, ListDificuldade.Count - 1)];
+            
         }
 
     }
--- Itens/Item.cs
+++ Itens/Item.cs
@@ -2,10 +2,28 @@ using System;
 
 namespace RPG_New.Itens
 {
-    class Item{
-
+    class Item
+    {
         public string Nome { get; private set; }
-        public int peso { get; private set; }
+        public int Tipo { get; private set; }//1 = Equip / 2 = Potion
+        public int Dificuldade { get; private set; } //Dificuldade de encontrar
+        public int Peso { get; private set; }
+        public int Forca { get; private set; }
+        public int Estamina { get; private set; }
+        public int Defesa { get; private set; }
+        public int Health { get; private set; }
+
+        public Item(string nome, int tipo,int dificuldade, int peso, int forca, int estamina, int defesa, int health)
+        {
+            Nome = nome;
+            Tipo = tipo;
+            Dificuldade = dificuldade;
+            Peso = peso;
+            Forca = forca;
+            Estamina = estamina;
+            Defesa = defesa;
+            Health = health;
+        }
 
         public override string ToString()
         {
